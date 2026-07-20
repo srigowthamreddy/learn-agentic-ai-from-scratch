2eb0cc8573ad9e00a971af90ca3d610effbd2d9a
Sri Gowtham Reddy Chirasani <chirasanisrigowthamreddy2025@gmail.com>
2026-07-20 21:35:46 +0530

    Delete Live-Class-2026-main directory

--- Live-Class-2026-main/Class 6  - Langchain Starts/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from class-6-langchain-starts!")
-
-
-if __name__ == "__main__":
-    main()
--- Live-Class-2026-main/README.md
+++
@@ -1,2 +0,0 @@
-# Live-Class-2026
-All resources for the Live class I take for year 2026
